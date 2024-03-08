baff54cceadf21d6149fad3f4c3745a3b9603799
ribesfas <154858293+ribesfas@users.noreply.github.com>
2024-03-08 13:38:33 +0900

    Update backend.java

--- pw/backend.java
+++ pw/backend.java
@@ -1,15 +1,25 @@
 package com.sopromadze.blogapi.utils;
 
-import org.bouncycastle.jce.provider.BouncyCastleProvider;
+import org.bouncycastle.jce.provider;
+/*Bouncy Castle : 보안 기능 프레임워크 */
+
+
 import org.bouncycastle.util.encoders.Hex;
 
 import javax.crypto.Cipher;
+/*javax.crypto 패키지에서 제공되는 Cipher 클래스 : 데이터를 암호화한다.  */
 import javax.crypto.spec.IvParameterSpec;
+/*초기화 백터를 지정하기 위한 클래스, 초기화 벡터를 생성하고 관리하는데 사용됨, 대칭 암호화*/
+
 import javax.crypto.spec.SecretKeySpec;
-import java.nio.charset.StandardCharsets;
+/*SecretKeySpec : 암호화 알고리즘에 사용될 비밀 키를 지정하기 위한 클래스 */
+import java.nio.charset.StandardCharsets; 
+/*StandardCharsets 문자 인코딩으로 되어 있다,  */
 import java.security.SecureRandom;
+/*java.util 패키지에서 제공, 보안 랜덤 바이트 생성.  */
 import java.security.Security;
 import java.util.Base64;
+/*java.util 패키지에서 제공되는 Base64 클래스 : 바이너리 데이터를 텍스트 형식으로 인코딩하거나 디코딩한다.  */
 
 public class AESUtils {
     private static final String ALGORITHM = "AES";
